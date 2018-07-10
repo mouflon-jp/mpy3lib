d7fb4c4fdb4af6b5a5e3e1a10ffbde8f2e8d4130
mouflon-jp <mouflon-jp@users.noreply.github.com>
2018-07-11 07:52:23 +0900

    ライブラリとして扱えるようにした

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="mpy3lib",
+    version="0.0.1",
+    author="mouflon-jp",
+    author_email="mouflon-jp@users.noreply.github.com",
+    description="A small python utils package",
+    license='MIT',
+    packages=setuptools.find_packages()
+)
